cea8d61fa5ca0f8e472301645972f057d91010f2
Alexander Sosnovsky <106810534+LeshenShow@users.noreply.github.com>
2024-11-19 15:18:21 +0300

    hw13

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,21 +2,27 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
-
-        default:
-            return state
+export const loadingReducer = (state: State = initState, action: LoadingActionType): State => {
+  switch (action.type) {
+    case "CHANGE_LOADING": {
+      return { ...state, isLoading: action.isLoading };
     }
-}
 
-type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+    default:
+      return state;
+  }
+};
 
-export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
+export const loadingAC = (isLoading: boolean): LoadingActionType =>
+  ({
+    type: "CHANGE_LOADING",
     isLoading,
-})
+  } as const);
+type LoadingActionType = {
+  type: "CHANGE_LOADING";
+  isLoading: boolean;
+};
+export type State = {
+  isLoading: boolean;
+  theme?: { themeId: number };
+};
